43f3ee839cd5b48cf21c7236ed2e1ae7c43cf8b3
ShihabYasin <shihabyasin@gmail.com>
2016-01-03 23:56:50 +0600

    presents the new exact traveling wave solutions of fourth order (1+1)- dimensional Boussinesq equation.
    Proposed a new exponential expansion method and
    apply to undertake this study. This study further shows some solitary
    and periodic waves
    graphically. This paper also shows that the novel exponential expansion
    method is easily
    applicable and powerful mathematical tool in the symbolic computational
    approach in the
    field of mathematical physics and engineering. The exact solutions of
    this equation play a
    vital role for describing different types of wave propagation in any
    varied natural instances.

---
+++ BSS EQN MATERIALS/program_1/u12_x_t.m
@@ -0,0 +1,66 @@
+clear all;
+V = 1;
+lamda = 1;
+xi = .5;
+mu =1;
+
+
+
+u(1,10) = 0;
+for x=-10:1:10
+for t=-10:1:10
+    if(lamda*mu > 0)
+        cosec = tan( sqrt(lamda*mu)*(x-V*t+xi)) ;
+    u(x+11,t+11) = -(1/6.0) - (1/3)*(lamda*lamda) - (mu*mu) + (1/6)*(V*V)-(2/3)*(lamda*lamda)*(cosec*cosec);
+    end
+end 
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+t=-10:1:10;
+
+subplot(1,2,1)
+
+meshz(t,t,u);
+hold on;
+
+meshc(t,t,u);
+hold on;
+
+surfc(t,t,u);
+hold on;
+
+waterfall(t,t,u);
+hold on;
+surfl(t,t,u);
+hold on;
+
+grid on;
+xlabel('t');
+zlabel('u(x,t)');
+ylabel('x');
+
+colormap hsv
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+subplot(1,2,2)
+% for t=0
+h = u(:,11);
+plot(h,'-.r*', 'LineWidth',2)
+
+hold on;
+% for t=2
+h = u(:,13);
+plot(h,'-*g', 'LineWidth',2)
+hold on;
+% for t=4
+h = u(:,15);
+plot(h,'-+b', 'LineWidth',2)
+hold on;
+grid on;
+
+legend('t=0','t=2','t=4');
+xlabel('x');
+ylabel('u(x)');
+
+
---
+++ BSS EQN MATERIALS/program_1/u7_x_t.m
@@ -0,0 +1,68 @@
+clear all;
+V = 1;
+lamda = -0.1;
+xi = .5;
+mu =0.1;
+
+
+
+u(1,10) = 0;
+for x=-10:1:10
+for t=-10:1:10
+    if(lamda<0 && mu >0)
+        cosec = 1/sin( sqrt(-lamda)*(x-V*t+xi)) ;
+    u(x+11,t+11) = (1/6.0)*(V*V) - (2/3)*(lamda) - (1/6) - 2*lamda*(cosec*cosec)      ;
+    end
+end 
+end
+
+
+t=-10:1:10;
+
+subplot(1,2,1)
+
+meshz(t,t,u);
+hold on;
+
+meshc(t,t,u);
+hold on;
+
+surfc(t,t,u);
+hold on;
+
+waterfall(t,t,u);
+hold on;
+surfl(t,t,u);
+hold on;
+
+grid on;
+xlabel('t');
+zlabel('u(x,t)');
+ylabel('x');
+
+colormap hsv
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+subplot(1,2,2)
+% for t=0
+h = u(:,11);
+plot(h,'-.r*', 'LineWidth',2)
+
+hold on;
+% for t=2
+h = u(:,13);
+plot(h,'-*g', 'LineWidth',2)
+hold on;
+% for t=4
+h = u(:,15);
+plot(h,'-+b', 'LineWidth',2)
+hold on;
+grid on;
+
+legend('t=0','t=2','t=4');
+xlabel('x');
+ylabel('u(x)');
+
+
+
+
